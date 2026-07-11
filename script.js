27498c95d7b9a1dbf4d6d75fd8f8eb28b7858f85
thesundaygk-pixel <thesundaygk@gmail.com>
2026-07-11 05:07:52 +0000

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+const navToggle = document.getElementById('navToggle');
+const siteNav = document.getElementById('siteNav');
+
+if (navToggle && siteNav) {
+  navToggle.addEventListener('click', () => {
+    siteNav.classList.toggle('show');
+  });
+}
